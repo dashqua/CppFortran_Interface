1d1d8582f0fb5fa4a470fd0695efdab6416252a6
Thomas Di Giusto <thomasdigiusto@me.com>
2018-03-14 14:47:03 +0100

    Project organisation v2

--- make.sh
+++ make.sh
@@ -1,9 +1,10 @@
 echo "  #CLEAN"
 rm *.o *.out *~ 2> /dev/null
+rm ./build/*.*
 echo "  #COMPILE OBJECTS"
-gfortran -c linalg.f90
-g++      -c main.cpp
+gfortran -c ./src/f90/linalg.f90 -o ./build/linalg.o
+g++      -c ./src/cpp/main.cpp   -o ./build/main.o
 echo "  #LINK OBJECTS"
-g++ -o test linalg.o main.o -lgfortran
+g++ -o test ./build/linalg.o ./build/main.o -lgfortran
 echo "  #EXEC"
 ./test
